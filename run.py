05970677e9e3012d6b39280678ff9e8fc9d6c54d
Arthur Araujo <arthuraraujo07@homtmail.com>
2024-09-13 16:47:27 -0300

    commit Procfile

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ from app import create_app
 # Cria a aplicação Flask
 app = create_app()
 
+# A função app.run() só deve ser usada no ambiente de desenvolvimento
 if __name__ == '__main__':
-    # Iniciar o servidor Flask
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False)  # Utilize debug=True apenas para desenvolvimento local
